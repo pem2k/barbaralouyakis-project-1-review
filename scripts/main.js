4d02f268a135d772caa4bdeaf864fc8c4b4feda9
Parker McKillop <107086485+pem2k@users.noreply.github.com>
2026-05-25 19:02:45 -0700

    Commented out the "message" variable assignment
    This was causing an eslint error as the "message" variable wasn't moved.

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
-    const message = document.getElementById("message").value.trim();
+    //const message = document.getElementById("message").value.trim();
 if (!name || !email) {
     feedback.textContent = "Please provide a name & email";
     feedback.classList.add("error");
